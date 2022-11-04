6841b0978d1c58bfd80073de9eef8cf428c1c668
Sakib5506 <38976669+Sakib5506@users.noreply.github.com>
2022-11-05 02:59:20 +0600

    div color togoller added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [editableNote, setEditableNote] = useState(null);
   const [editMode, setEditMode] = useState(false);
 
+  
   const noteCreateHandler = (event) =>{
     event.preventDefault();
     if(title){
@@ -23,6 +24,7 @@ function App() {
       alert('Please enter a valid title');
     }
   }
+
   const noteDeleteHandler = (noteId) =>{
     setNotes(notes.filter((note) => note.id !== noteId))
   }
@@ -50,19 +52,35 @@ function App() {
     }
   }
 
+  const handleClick = (is) =>{
+    setNotes(notes.map((item) =>{
+      if(item.id === is){
+        item.isComplete = !item.isComplete;
+      }
+      return item;
+    }))
+      
+  }
   return (
     <div className="App">
-      <form onSubmit = { (event) => editMode ? updateHandler(event) : noteCreateHandler(event)}>
+      <form onSubmit = { (event) => editMode ? updateHandler(event) : noteCreateHandler(event)} style={{display:'flex', alignItems: 'center', justifyContent:'center'}}>
         <input onChange={(event) => setTitle(event.target.value)} type="text" value = {title} name="" id="" placeholder='Enter your note'/>
         <button type="submit">{editMode? 'Update Note' : 'Add Note'}</button>
       </form>
-      <ul className='list-area'>
-        {notes.map((note) =>(
+      
+      <ul className='list-area' style={{textAlign: 'center', paddingRight: '350px', paddingLeft:'350px'}}>
+        {notes.map((note) =>( 
+          <div id='div-toggler' onClick={() =>handleClick(note.id)} style={{border: '1px solid black', height: '50px', margin: '10px', backgroundColor: note.isComplete? 'red' : 'blue',color: 'whitesmoke'}}>   
           <li>
-            <span>{note.title}</span>
-            <button onClick = {() => editHandler(note.id)}>Edit</button>
-            <button onClick={() => noteDeleteHandler(note.id)}>Delete</button>
-          </li>
+            <span>{note.title}</span><br />
+            <button onClick = {(e) => {
+              e.stopPropagation();
+              editHandler(note.id)}}>Edit</button>
+            <button onClick={(e) => {
+              e.stopPropagation();
+              noteDeleteHandler(note.id)}}>Delete</button>
+          </li>   
+          </div>       
         ))}
       </ul>
     </div>
