84fca4267e76666e6af74585efbe0654510eb95c
Sakib5506 <38976669+Sakib5506@users.noreply.github.com>
2022-11-09 01:44:58 +0600

    New fix

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
         setNotes = {setNotes}
         editableNote = {editableNote}
         setEditableNote = {setEditableNote}
+        editMode = {editMode}
         setEditMode = {setEditMode}
       />
 
